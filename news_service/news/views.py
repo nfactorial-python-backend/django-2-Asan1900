9d1379f01cd90cfda7f75dfe4717d226df65f80f
abdr <asan.abdirakhym@gmail.com>
2023-08-13 12:20:28 +0600

    list, detail and news update func

---
+++ news_service/news/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import News, Comment
+
+def news_list(request):
+    news_list = News.objects.all().order_by('-created_at')
+    return render(request, 'news/news_list.html', {'news_list': news_list})
+
+def news_detail(request, news_id):
+    news = get_object_or_404(News, pk=news_id)
+    comments = Comment.objects.filter(news=news)
+    
+    if request.method == 'POST':
+        content = request.POST['content']
+        Comment.objects.create(news=news, content=content)
+        return redirect('news_detail', news_id=news_id)
+    
+    return render(request, 'news/news_detail.html', {'news': news, 'comments': comments})
+    
+def add_news(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        news = News.objects.create(title=title, content=content)
+        return redirect('news_detail', news_id=news.pk)
+    
+    return render(request, 'news/add_news.html')
