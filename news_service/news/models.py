c4197d480b18e73759ba513ec2f5ad480e877116
abdr <asan.abdirakhym@gmail.com>
2023-08-13 12:21:44 +0600

    class news and comments

---
+++ news_service/news/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
